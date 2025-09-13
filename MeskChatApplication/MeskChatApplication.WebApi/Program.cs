ebbe1db02efc3a41dc217c0f47f9ffcf0764f214
mehmet.solak <solakmehmet02@gmail.com>
2025-09-13 21:36:56 +0300

    update cors origins, added localhost:3000

--- MeskChatApplication/MeskChatApplication.WebApi/Program.cs
+++ MeskChatApplication/MeskChatApplication.WebApi/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAll", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("http://localhost:5173", "http://localhost:3000")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
