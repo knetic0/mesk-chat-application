94cf93dd31a0d2b00bdfc5060778cf46a94b0dfe
mehmet.solak <solakmehmet02@gmail.com>
2025-09-16 00:52:38 +0300

    Refactor signalR client to use CustomHttpClient for improved token handling and request management

--- MeskChatClient/src/signalr.ts
+++ MeskChatClient/src/signalr.ts
@@ -1,10 +1,45 @@
 import * as signalR from "@microsoft/signalr";
+import { refreshTokenAsync } from "./axios";
 
 const baseURL = import.meta.env.VITE_API_URL;
 
+const getAuthHeaders = () => {
+  return {
+    Authorization: `Bearer ${localStorage.getItem("accessToken") || ""}`,
+  };
+};
+
+class CustomHttpClient extends signalR.DefaultHttpClient {
+  constructor() {
+    super(console);
+  }
+
+  public async send(request: signalR.HttpRequest): Promise<signalR.HttpResponse> {
+    const authHeaders = getAuthHeaders();
+    request.headers = { ...request.headers, ...authHeaders };
+    try {
+      const response = await super.send(request);
+      return response;
+    } catch (er) {
+      if(er instanceof signalR.HttpError) {
+        const error = er as signalR.HttpError;
+        if (error.statusCode === 401) {
+          await refreshTokenAsync();
+          const newAuthHeaders = getAuthHeaders();
+          request.headers = { ...request.headers, ...newAuthHeaders };
+        }
+      } else {
+        throw er;
+      }
+    }
+    return super.send(request);
+  }
+}
+
+
 export const connection = new signalR.HubConnectionBuilder()
   .withUrl(`${baseURL}/chat`, {
-    accessTokenFactory: () => localStorage.getItem("accessToken") || "",
+    httpClient: new CustomHttpClient(),
   })
   .withAutomaticReconnect()
   .build();
